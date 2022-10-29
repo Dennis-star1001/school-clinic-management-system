c3f47840d3bf47119c0f29aba70550bd20f1a768
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-10-29 13:49:08 +0100

    Hot fix

--- Controller/Classes/patient.php
+++ Controller/Classes/patient.php
@@ -2,17 +2,20 @@
 class Patient extends Database
 {
     //Patience datas 
-    private $name;
-    private $age;
-    private $contact;
-    private $department;
-    private $matric_no;
-    private $level;
-    private $course;
+    public $name;
+    public $age;
+    public $email;
+    public $department;
+    public $level;
+    public $genotype;
+    public $blood_group;
+    public $sickness;
+    public $address;
+    public $faculty;
 
 
     //Patience table
-    private $patient_table = '';
+    private $patient_table = 'patient';
 
 
     //View patience info
@@ -39,12 +42,34 @@ class Patient extends Database
     public function patientsValidation()
     {
         if (Fun::checkForEmptyInput([$this->name, $this->age, $this->contact, $this->department, $this->matric_no, $this->level, $this->course])) {
-            Fun::redirect("", "", "");
+            Fun::redirect("../../View/patient.php", "err", "None of the fields must be empty");
+            exit;
         }
+        Fun::redirect("../../View/patient.php", "success", "Data has been saved");
     }
+
+    public function processPatientInfo($name,$email, $blood_group, $genotype, $sickness,$address, $department, $program, $faculty, $age, $level)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->blood_group = $blood_group;
+        $this->genotype = $genotype;
+        $this->sickness = $sickness;
+        $this->address = $address;
+        $this->department = $department;
+        $this->program = $program;
+        $this->age = $age;
+        $this->faculty = $faculty;
+        $this->level = $level;
+        $this->patientsValidation();
+        $this->savePatientInfo();
+    }
+
     //Save patience info imto the database
     public function savePatientInfo()
     {
-        return $this->save($this->patient_table, "");
+        echo 'name';
+        exit;
+        return $this->save($this->patient_table, "name='$this->name', email='$this->email', blood_group='$this->blood_group', genotype='$this->genotype', sickness='$this->sickness', address='$this->address', department='$this->department', program='$this->program', faculty='$this->faculty', age='$this->age', level='$this->level'");
     }
 }
